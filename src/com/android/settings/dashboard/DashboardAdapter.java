24fe319a8d0413ffe0e235ea8097ae6692a43836
Alex <mazdarider23@gmail.com>
2016-09-12 17:52:36 -0400

    Remove dashboard tile summaries [1/2]
    Change-Id: I6fab69dc56b29762add78fa7b118a63b8f08b33c

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -130,8 +130,14 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
 
     public void setCategoriesAndSuggestions(List<DashboardCategory> categories,
             List<Tile> suggestions) {
-        mSuggestions = suggestions;
-        recountItems();
+        if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
+             mSuggestions = suggestions;
+             recountItems();
+        } else {
+             mSuggestions = null;
+             recountItems();
+        }
     }
 
     public Tile getTile(ComponentName component) {
@@ -186,15 +192,6 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
     }
 
     public void setCategories(List<DashboardCategory> categories) {
-        if ((Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
-             mSuggestions = suggestions;
-             recountItems();
-        } else {
-             mSuggestions = null;
-             recountItems();
-        }
-
         mCategories = categories;
 
         TypedValue tintColorValue = new TypedValue();
